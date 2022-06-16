c4698639b1ba74d9402bb6a06fae03adc7de22a9
Lia Moua <moua0061@yahoo.com>
2022-06-16 18:57:11 -0400

    added header

--- H03_61.java
+++ H03_61.java
@@ -1,3 +1,13 @@
+/** CSE205 Object Oriented Programming and Data Structures, Summer 2022 C-Session
+Homework 3
+
+Group 4
+AUTHOR: David McConnell  dmcconn7    dmcconn7@asu.edu
+ AUTHOR: Lia Moua         amoua       amoua@asu.edu
+ AUTHOR: Arsal Akhtar     akakhta2    akakhta2@asu.edu
+ AUTHOR: Kari McBride     kemcbri2    kemcbri2@asu.edu
+ */
+
 public class H03_61 {
     public class Point implements Comparable<Point> {
         private double mX;
